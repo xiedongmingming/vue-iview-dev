4c09579833475ea28ecf461a9aa0546d3f84136a
xieming <xieming@adwetec.com>
2020-11-14 18:40:50 +0800

    学习

--- scripts/server.js
+++ scripts/server.js
@@ -3,12 +3,12 @@
  */
 
 const tables = [
-    'Entitymanager',
-    'Entityfield',
-    'Menu',
-    'ModuleEditor',
-    'I18n',
-    'App',
+	'Entitymanager',
+	'Entityfield',
+	'Menu',
+	'ModuleEditor',
+	'I18n',
+	'App',
 ];
 let Handlebars = require('handlebars');
 let moment = require('moment');
@@ -22,7 +22,7 @@ let low = require('lowdb');
 let _ = require('lodash');
 
 let db = low('mock/db_yj.json', {
-    storage: fileAsync
+	storage: fileAsync
 })
 
 let app = express();
@@ -31,281 +31,281 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 Handlebars.registerHelper('if_eq', function (v1, v2, opts) {
-    if (v1 === v2)
-        return opts.fn(this);
-    else
-        return opts.inverse(this);
+	if (v1 === v2)
+		return opts.fn(this);
+	else
+		return opts.inverse(this);
 });
 Handlebars.registerHelper('if_neq', function (v1, v2, opts) {
-    if (v1 !== v2) {
-        return opts.fn(this);
-    } else
-        return opts.inverse(this);
+	if (v1 !== v2) {
+		return opts.fn(this);
+	} else
+		return opts.inverse(this);
 });
 
 Handlebars.registerHelper('if_indexOf', function (v1, v2, opts) {
-    if (v1.indexOf(v2) > -1) {
-        return opts.fn(this);
-    } else
-        return opts.inverse(this);
+	if (v1.indexOf(v2) > -1) {
+		return opts.fn(this);
+	} else
+		return opts.inverse(this);
 });
 
 function success(data) {
-    return {
-        success: true,
-        content: data
-    }
+	return {
+		success: true,
+		content: data
+	}
 }
 
 function fail(err) {
-    return {
-        success: false,
-        errorMsg: err,
-        content: data,
-    };
+	return {
+		success: false,
+		errorMsg: err,
+		content: data,
+	};
 }
 
 function log() {
-    if (true)
-        console.log(_.values(arguments));
+	if (true)
+		console.log(_.values(arguments));
 }
 
 function mockData() {
-    //init db
-    var dbs = {};
-    _.forEach(tables, (t) => _.assign(dbs, {[t]: []}))
-    db.defaults(dbs).write();
+	//init db
+	var dbs = {};
+	_.forEach(tables, (t) => _.assign(dbs, {[t]: []}))
+	db.defaults(dbs).write();
 
-    db.defaults().forEach((m, v) => {
-        tables.push(v);
-    }).value();
+	db.defaults().forEach((m, v) => {
+		tables.push(v);
+	}).value();
 
-    //init uri
-    try {
-        tables.map(t => {
-            log(`init ${t}`);
-            app.use('/api/' + t.toLowerCase() + '/:id', function (req, res) {
-                log("req.params.id=" + req.params.id);
-                if (req.method === 'GET') {
-                    const post = db.get(t)
-                        .find({id: _.parseInt(req.params.id)})
-                        .value()
-                    res.send(success(post))
-                }
-            });
-            app.use('/api/' + t.toLowerCase(), function (req, res) {
-                if (req.method === 'GET') {
+	//init uri
+	try {
+		tables.map(t => {
+			log(`init ${t}`);
+			app.use('/api/' + t.toLowerCase() + '/:id', function (req, res) {
+				log("req.params.id=" + req.params.id);
+				if (req.method === 'GET') {
+					const post = db.get(t)
+							.find({id: _.parseInt(req.params.id)})
+							.value()
+					res.send(success(post))
+				}
+			});
+			app.use('/api/' + t.toLowerCase(), function (req, res) {
+				if (req.method === 'GET') {
 
-                    let {sortOrder, pageSize, current, sortField} = req.query;
-                    let condition = _.omit(req.query, ['sortOrder', 'pageSize', 'current', 'sortField'])
-                    let value = db.get(t);
-                    let total = 0;
-                    if (_.keys(condition).length != 0) {
-                        log('condition:', JSON.stringify(condition))
-                        condition = _.mapValues(condition, function (m) {
-                            var reg = /^[0-9]*$/;
-                            if (reg.test(m))
-                                return m * 1;
-                            else
-                                return m;
-                        });
-                        condition = _.omit(condition,'lang')
-                        value = value.filter(condition)
-                    }
-                    total = value.size();
-                    if (sortField) {
-                        log('sortBy:', sortField)
-                        value = value.sortBy(sortField)
-                    }
+					let {sortOrder, pageSize, current, sortField} = req.query;
+					let condition = _.omit(req.query, ['sortOrder', 'pageSize', 'current', 'sortField'])
+					let value = db.get(t);
+					let total = 0;
+					if (_.keys(condition).length != 0) {
+						log('condition:', JSON.stringify(condition))
+						condition = _.mapValues(condition, function (m) {
+							var reg = /^[0-9]*$/;
+							if (reg.test(m))
+								return m * 1;
+							else
+								return m;
+						});
+						condition = _.omit(condition, 'lang')
+						value = value.filter(condition)
+					}
+					total = value.size();
+					if (sortField) {
+						log('sortBy:', sortField)
+						value = value.sortBy(sortField)
+					}
 
-                    if (sortOrder && (sortOrder === 'ascend' || sortOrder === 'asc')) {
-                        log('sortOrder:', sortOrder);
-                        value = value.reverse()
-                    }
+					if (sortOrder && (sortOrder === 'ascend' || sortOrder === 'asc')) {
+						log('sortOrder:', sortOrder);
+						value = value.reverse()
+					}
 
-                    if (pageSize && current) {
-                        log('pageSize:', pageSize)
-                        value = value.slice((current - 1) * pageSize, current * pageSize);
-                        res.send(success({
-                            current: _.parseInt(current),
-                            total: total,
-                            list: value.value()
-                        }))
-                    } else {
-                        res.send(success(value.value()))
-                    }
+					if (pageSize && current) {
+						log('pageSize:', pageSize)
+						value = value.slice((current - 1) * pageSize, current * pageSize);
+						res.send(success({
+							current: _.parseInt(current),
+							total: total,
+							list: value.value()
+						}))
+					} else {
+						res.send(success(value.value()))
+					}
 
-                } else if (req.method === 'POST') {
-                    db.get(t)
-                        .push(req.body)
-                        .last()
-                        .assign({id: Date.now()})
-                        .write()
-                        .then(post => res.send(success(post)))
-                } else if (req.method === 'PUT') {
-                    db.get(t)
-                        .find({id: req.body.id})
-                        .assign(req.body)
-                        .write()
-                        .then(post => res.send(success(post)))
-                } else if (req.method === 'DELETE') {
-                    if (!req.body.id) {
-                        res.send(fail('not find id!'))
-                    } else if (_.isNumber(req.body.id)) {
-                        db.get(t)
-                            .remove({id: req.body.id})
-                            .write()
-                            .then(post => res.send(success(post)))
-                    } else if (req.body.id === 'all') {
-                        db.get(t).remove(() => 1 === 1).write().then(post => res.send(success(post)))
-                    } else {
-                        let ids = req.body.id.split(",");
-                        db.get(t).remove((m) => {
-                            return ids.indexOf(m.id + "") > -1;
-                        }).write().then(post => res.send(success(post)))
-                    }
+				} else if (req.method === 'POST') {
+					db.get(t)
+							.push(req.body)
+							.last()
+							.assign({id: Date.now()})
+							.write()
+							.then(post => res.send(success(post)))
+				} else if (req.method === 'PUT') {
+					db.get(t)
+							.find({id: req.body.id})
+							.assign(req.body)
+							.write()
+							.then(post => res.send(success(post)))
+				} else if (req.method === 'DELETE') {
+					if (!req.body.id) {
+						res.send(fail('not find id!'))
+					} else if (_.isNumber(req.body.id)) {
+						db.get(t)
+								.remove({id: req.body.id})
+								.write()
+								.then(post => res.send(success(post)))
+					} else if (req.body.id === 'all') {
+						db.get(t).remove(() => 1 === 1).write().then(post => res.send(success(post)))
+					} else {
+						let ids = req.body.id.split(",");
+						db.get(t).remove((m) => {
+							return ids.indexOf(m.id + "") > -1;
+						}).write().then(post => res.send(success(post)))
+					}
 
-                }
-            });
+				}
+			});
 
-        });
-    } catch (e) {
-        console.error(e);
-    }
+		});
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 app.all('/api/codecreator', function (req, res) {
-    if (req.method === 'POST') {
-        let entitys = db.get('Entitymanager').filter((e) => {
-            return _.indexOf(req.body.entitys, e.id) > -1
-        }).value();
+	if (req.method === 'POST') {
+		let entitys = db.get('Entitymanager').filter((e) => {
+			return _.indexOf(req.body.entitys, e.id) > -1
+		}).value();
 
-        let results = [];
+		let results = [];
 
-        ['Action', 'Grid'].forEach(n => {
-            let module = db.get('ModuleEditor').filter((e) => e.type === n).value();
-            log(module)
-            if (module.length > 0) {
-                const template = Handlebars.compile(module[0].code);
-                _.forEach(entitys, (e) => {
+		['Action', 'Grid'].forEach(n => {
+			let module = db.get('ModuleEditor').filter((e) => e.type === n).value();
+			log(module)
+			if (module.length > 0) {
+				const template = Handlebars.compile(module[0].code);
+				_.forEach(entitys, (e) => {
 
-                    let fields = db.get('Entityfield').filter((f) => {
-                        return f.entityId === e.id;
-                    }).sortBy('order').value();
+					let fields = db.get('Entityfield').filter((f) => {
+						return f.entityId === e.id;
+					}).sortBy('order').value();
 
-                    let selectFields = _.chain(fields).filter(f => {
-                        return ( f.type === 'select' || f.type === 'radio' || f.type === 'checkbox' ) && !!f.select_option
-                    }).uniqBy(f => f.select_option).value();
+					let selectFields = _.chain(fields).filter(f => {
+						return (f.type === 'select' || f.type === 'radio' || f.type === 'checkbox') && !!f.select_option
+					}).uniqBy(f => f.select_option).value();
 
 
-                    let config = {
-                        NAME: e.name,
-                        zh_name: e.zh_name,
-                        lname: e.name.toLowerCase(),
-                        fields: fields,
-                        selectFields: selectFields,
-                        user: os.hostname(),
-                        date: moment(new Date()).format('YYYY-MM-DD')
-                    };
-                    let result = template(config);
-                    log(result)
-                    let directory = `${__dirname}/../src/${module[0].route}/${e.route}/`;
-                    if (!fs.existsSync(directory)) {
-                        fs.mkdirSync(directory);
-                    }
-                    db.defaults({[e.name]: []})
-                        .write();
-                    fs.writeFileSync(`${directory}${e.name}.js`, result);
-                    results.push({
-                        status: 'check',
-                        path: `/src/${module[0].route}/${e.route}/${e.name}.js`
-                    })
-                    mockData();
-                })
-            }
-        })
+					let config = {
+						NAME: e.name,
+						zh_name: e.zh_name,
+						lname: e.name.toLowerCase(),
+						fields: fields,
+						selectFields: selectFields,
+						user: os.hostname(),
+						date: moment(new Date()).format('YYYY-MM-DD')
+					};
+					let result = template(config);
+					log(result)
+					let directory = `${__dirname}/../src/${module[0].route}/${e.route}/`;
+					if (!fs.existsSync(directory)) {
+						fs.mkdirSync(directory);
+					}
+					db.defaults({[e.name]: []})
+							.write();
+					fs.writeFileSync(`${directory}${e.name}.js`, result);
+					results.push({
+						status: 'check',
+						path: `/src/${module[0].route}/${e.route}/${e.name}.js`
+					})
+					mockData();
+				})
+			}
+		})
 
-        res.json({
-            success: true,
-            content: results
-        });
-    }
+		res.json({
+			success: true,
+			content: results
+		});
+	}
 });
 app.all('/api/codeeditor', function (req, res) {
-    if (req.method === 'GET') {
-        let {path = null} = req.query;
-        if (!!path) {
-            let fileContent = fs.readFileSync(`${__dirname}/../src${path}`, 'utf-8');
-            res.json({
-                success: true,
-                content: {
-                    fileContent
-                }
-            })
-        }
-    } else if (req.method === 'POST') {
-        let {path, fileContent} = req.body;
-        fs.writeFileSync(`${__dirname}/../src${path}`, fileContent);
-        res.json({
-            success: true,
-            content: {
-                fileContent,
-                path
-            }
-        })
-    }
+	if (req.method === 'GET') {
+		let {path = null} = req.query;
+		if (!!path) {
+			let fileContent = fs.readFileSync(`${__dirname}/../src${path}`, 'utf-8');
+			res.json({
+				success: true,
+				content: {
+					fileContent
+				}
+			})
+		}
+	} else if (req.method === 'POST') {
+		let {path, fileContent} = req.body;
+		fs.writeFileSync(`${__dirname}/../src${path}`, fileContent);
+		res.json({
+			success: true,
+			content: {
+				fileContent,
+				path
+			}
+		})
+	}
 });
 app.all('/api/login', function (req, res) {
-    log(req.query)
-    setTimeout(() => {
-        if (req.query.username === 'admin' && req.query.password === '000000') {
+	log(req.query)
+	setTimeout(() => {
+		if (req.query.username === 'admin' && req.query.password === '000000') {
 
-            res.json({
-                success: true,
-                content: {'username': 'admin', id: 1, token: ''}
-            });
-        } else {
-            res.json({
-                success: false,
-                errorMsg: 'username or password wrong !'
-            });
-        }
-    }, 2000);
+			res.json({
+				success: true,
+				content: {'username': 'admin', id: 1, token: ''}
+			});
+		} else {
+			res.json({
+				success: false,
+				errorMsg: 'username or password wrong !'
+			});
+		}
+	}, 2000);
 });
 
 app.all('/api/auth', function (req, res) {
-    log(req.query)
-    setTimeout(() => {
-        if (req.query.username === 'admin' && req.query.password === '000000') {
-            res.json({
-                success: true,
-                content: {'username': 'admin', id: 1, token: 'virtualToken'}
-            });
-        } else {
-            res.json({
-                success: false,
-                errorMsg: 'username or password wrong !'
-            });
-        }
-    }, 2000);
+	log(req.query)
+	setTimeout(() => {
+		if (req.query.username === 'admin' && req.query.password === '000000') {
+			res.json({
+				success: true,
+				content: {'username': 'admin', id: 1, token: 'virtualToken'}
+			});
+		} else {
+			res.json({
+				success: false,
+				errorMsg: 'username or password wrong !'
+			});
+		}
+	}, 2000);
 });
 
 app.all('/api/auth/user', function (req, res) {
-    res.json({
-        success: true,
-        content: {'username': 'admin', id: 1, 'firstname': 'admin', 'lastname': 'admin'}
-    });
+	res.json({
+		success: true,
+		content: {'username': 'admin', id: 1, 'firstname': 'admin', 'lastname': 'admin'}
+	});
 });
-
 
 mockData();
+
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
 
-app.listen('8000');
+app.listen('8010');
--- src/router.js
+++ src/router.js
@@ -1,86 +1,95 @@
 import Pages from './components/pages';
 
-const routers = [
-    {
-        path: '/',
-        redirect: '/incidents'
-    },
-    {
-        path: '/Login',
-        name: 'name',
-        component: Pages.Login,
-    }, {
-        path: '*', component: Pages.Home
-    }, {
-        path: '/home',
-        name: 'Home',
-        component: Pages.Home,
-        children: [
-            {
-                name: 'Incidents',
-                path: '/incidents',
-                component: Pages.Incidents,
-            }, {
-                name: 'Alerts',
-                path: '/alerts',
-                component: Pages.Alerts,
-            }, {
-                name: 'Configuration',
-                path: '/configuration',
-                component: Pages.Nothing,
-                children: [
-                    {
-                        name: 'Schedules',
-                        path: '/schedules',
-                        component: Pages.Schedules
-                    }, {
-                        name: 'Schedules',
-                        path: '/schedules/:id/edit',
-                        component: Pages.SchedulesEdit
-                    }, {
-                        name: 'Services',
-                        path: '/services',
-                        component: Pages.Services,
-                        children: []
-                    }, {
-                        name: 'Service Detail',
-                        path: '/services/:id',
-                        component: Pages.ServiceDetail,
-                    }, {
-                        name: 'Add Service',
-                        path: '/service_add',
-                        component: Pages.AddService
-                    }, {
-                        name: 'Edit Service',
-                        path: '/service_edit/:id',
-                        component: Pages.AddService
-                    }, {
-                        name: 'Users',
-                        path: '/users',
-                        component: Pages.Users
-                    }, {
-                        name: 'Users',
-                        path: '/users/:id',
-                        component: Pages.UserDetail
-                    }, {
-                        name: 'Teams',
-                        path: '/teams',
-                        component: Pages.Teams
-                    },
-                ]
-            }, {
-                name: 'Analytics',
-                path: '/analytics',
-                component: Pages.Nothing,
-                children: [
-                    {
-                        name: 'demo',
-                        path: '/demo',
-                        component: Pages.UsersDemo
-                    }
-                ]
-            }
-        ]
-    },
+// 路由配置参数 -- 每一个路由映射一个组件
+const routers = [// WEBPACK会把每一个路由都打包为一个JS文件在请求到该页面时才去加载这个页面的JS
+	{
+		path: '/',
+		redirect: '/incidents'
+	},
+	{
+		path: '/Login',
+		name: 'name',
+		meta: {
+			title: '登录'
+		},
+		component: Pages.Login,// 对应的组件
+		// component: (resolve) => require(['./views/index.vue'], resolve) 异步懒加载
+		// component: require('./views/index.vue')	// 一次性加载
+	}, {
+		path: '*', component: Pages.Home// 访问路径不存在时重定向到首页
+	}, {
+		path: '/home',
+		name: 'Home',
+		meta: { // 统一配置一些自定义信息(如标题)
+			title: '主页'
+		},
+		component: Pages.Home,
+		children: [
+			{
+				name: 'Incidents',
+				path: '/incidents',
+				component: Pages.Incidents,
+			}, {
+				name: 'Alerts',
+				path: '/alerts',
+				component: Pages.Alerts,
+			}, {
+				name: 'Configuration',
+				path: '/configuration',
+				component: Pages.Nothing,
+				children: [
+					{
+						name: 'Schedules',
+						path: '/schedules',
+						component: Pages.Schedules
+					}, {
+						name: 'Schedules',
+						path: '/schedules/:id/edit',
+						component: Pages.SchedulesEdit
+					}, {
+						name: 'Services',
+						path: '/services',
+						component: Pages.Services,
+						children: []
+					}, {
+						name: 'Service Detail',
+						path: '/services/:id',
+						component: Pages.ServiceDetail,
+					}, {
+						name: 'Add Service',
+						path: '/service_add',
+						component: Pages.AddService
+					}, {
+						name: 'Edit Service',
+						path: '/service_edit/:id',
+						component: Pages.AddService
+					}, {
+						name: 'Users',
+						path: '/users',
+						component: Pages.Users
+					}, {
+						name: 'Users',
+						path: '/users/:id',
+						component: Pages.UserDetail
+					}, {
+						name: 'Teams',
+						path: '/teams',
+						component: Pages.Teams
+					},
+				]
+			}, {
+				name: 'Analytics',
+				path: '/analytics',
+				component: Pages.Nothing,
+				children: [
+					{
+						name: 'demo',
+						path: '/demo',
+						component: Pages.UsersDemo
+					}
+				]
+			}
+		]
+	},
 ];
 export default routers;
